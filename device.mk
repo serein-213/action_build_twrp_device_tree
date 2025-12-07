27a92644df0090671183fa40c6c3af7e9115ce35
serein-213 <1246452649@qq.com>
2025-12-07 12:58:31 +0800

    fix: clear misc partition properly to prevent bootloop

--- device.mk
+++ device.mk
@@ -6,3 +6,14 @@
 #
 
 LOCAL_PATH := device/oppo/PCGM00
+
+# TWRP: include QCOM decryption helpers
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+# Copy init scripts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery/root/init.recovery.qcom.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.qcom.rc \
+    $(LOCAL_PATH)/recovery/root/init.recovery.vold_decrypt.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.vold_decrypt.rc \
+    $(LOCAL_PATH)/recovery/root/ueventd.qcom.rc:$(TARGET_COPY_OUT_RECOVERY)/root/ueventd.qcom.rc
